137b99dd72166cc1cedf102c2a2b800d2d40748e
An <2103887953@qq.com>
2017-08-30 17:41:28 +0800

    add goodmsg

--- goodMsg.js
+++ goodMsg.js
@@ -1,12 +1,33 @@
 #!/usr/bin/env node
+const fs = require('fs')
+
 var run= function (obj) {
-  if(obj[1] === '-v'){
-    console.log('version is 1.0.0');
-  }
-  if(obj[1] === '-h'){
-    console.log('Useage:');
-    console.log('  -v --version [show version]');
-  }
+	let name = obj[0]
+	let address = obj[1]
+	if(name && address) {
+		const fileName = './goodMsg.md'
+	  	const readAble = fs.createReadStream(fileName)
+		var body = ''
+		readAble.on('data', (chunk) => {
+		  body += chunk
+		})
+		readAble.on('end', () => {
+			if(~body.indexOf(address)) {
+				console.log('已分享过本篇文章！')
+				return null
+			} else {
+				body += `[${name}](${address})`
+				body += '\n'
+				fs.writeFile(fileName, body, (err) => {
+				  	if (err) throw err;
+				  	console.log('文件：'+ fileName.slice(2) +' 已经更新')
+				});
+			}
+		})
+	} else {
+		console.log('请输入完整参数')
+	}
+	
 };
 //获取除第一个命令以后的参数，使用空格拆分
 run(process.argv.slice(2)); 
---
+++ goodMsg.md
@@ -0,0 +1,4 @@
+## 文章分享
+[JS正则表达式完整教程](https://juejin.im/post/5965943ff265da6c30653879)
+[Node.js垃圾回收](https://eggggger.xyz/2016/10/22/node-gc/)
+[可视化正则工具](http://regexr.com/)

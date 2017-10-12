76dd8c5030f2beef832f0167df62d14709dfdbe5
Aaaaaaaty <2103887953@qq.com>
2017-10-12 20:10:57 +0800

    add zip unzip

--- fsPathSys/fs.js
+++ fsPathSys/fs.js
@@ -1,109 +1,111 @@
-const fs = require('fs')
-let path = process.cwd() + '/testui'
-let dataKey = ['.js', '.css', '.html']
-let data = [
-	{	
-		'type': 'script',
-		'point': 'www.script.com/'
-	},
-	{	
-		'type': 'link',
-		'point': 'www.css.com/'
-	},
-	{	
-		'type': 'img',
-		'point': 'www.img.com/'
-	},
-	{	
-		'type': 'background',
-		'point': 'www.bg.com/'
-	}
-]
-
-function fsPathSys(path, dataKey) { //遍历路径
-	fs.readdir(path, isDirectory)
-	function isDirectory(err, files) {
-		if(err) {
-			return err
-		} else {
-			files.forEach((item, index) => {
-				let nowPath = `${path}/${item}`
-				let stat = fs.statSync(nowPath)
-				if(!stat.isDirectory()) {
-					dataKey.forEach((obj, index) => {
-						if(~item.indexOf(obj)) {
-							replaceAddress(nowPath)
-						}
-					})
-				} else {		
-					fsPathSys(nowPath, dataKey)
-				}
-			})
+// const fs = require('fs')
+function fsPathRepeat(path) {
+	console.log(path)
+	let dataKey = ['.js', '.css', '.html']
+	let data = [
+		{	
+			'type': 'script',
+			'point': 'www.script.com/'
+		},
+		{	
+			'type': 'link',
+			'point': 'www.css.com/'
+		},
+		{	
+			'type': 'img',
+			'point': 'www.img.com/'
+		},
+		{	
+			'type': 'background',
+			'point': 'www.bg.com/'
 		}
-	}
-}
-
-function replaceAddress(path) {
-	let readAble = fs.createReadStream(path)
-	let body = ''
-	readAble.on('data', (chunk) => {
-	  body += chunk
-	})
-	readAble.on('end', () => {
-		matchData(path, data, body)
-	})
-}
-
-
-function matchData(path, data, body) {
-	let replaceBody = {}
-	data.forEach((obj, i) => {
-		if(obj.type === 'script' || obj.type === 'link' || obj.type === 'img') {
-			let bodyMatch = body.match(new RegExp(`<${obj.type}.*?>`, 'g'))
-			if(bodyMatch) {
-				bodyMatch.forEach((item, index) => {
-					let itemMatch = item.match(/(src|href)\s*=\s*["|'].*?["|']/g)
-					if(itemMatch) {
-						itemMatch.forEach((data, i) => {
-							let matchItem = data.match(/(["|']).*\//g)[0].replace(/\s/g, '').slice(1)
-							if(!replaceBody[matchItem]) {
-								replaceBody[matchItem] = obj.point
+	]
+	function fsPathSys(path, dataKey) { //遍历路径
+		fs.readdir(path, isDirectory)
+		function isDirectory(err, files) {
+			if(err) {
+				return err
+			} else {
+				files.forEach((item, index) => {
+					let nowPath = `${path}/${item}`
+					let stat = fs.statSync(nowPath)
+					if(!stat.isDirectory()) {
+						dataKey.forEach((obj, index) => {
+							if(~item.indexOf(obj)) {
+								replaceAddress(nowPath)
 							}
 						})
+					} else {		
+						fsPathSys(nowPath, dataKey)
 					}
 				})
 			}
-		} else if(obj.type === 'background') {
-			let bodyMatch = body.match(/url\(.*?\)/g)
-			if(bodyMatch) {
-				bodyMatch.forEach((item, index) => {
-					let itemMatch = item.match(/\(.*\//g)[0].replace(/\s/g, '').slice(1)
-					if(!replaceBody[itemMatch]) {
-						replaceBody[itemMatch] = obj.point
-					}
-				})
-			}
-
 		}
-	})
-	replaceSepical(path, body, replaceBody)	
-}
+	}
 
-function replaceSepical(path, body, replaceBody) {
-	Object.keys(replaceBody).sort((a,b) => b.length - a.length).forEach((item, index) => {
-		let i = item,
-			itemReplace
-		if(item.match(/\.\//g)) {
-			item = item.replace(/\./g, '\\\.')
-		} 
-		itemReplace = new RegExp(item, 'g')	
-		body = body.replace(itemReplace, replaceBody[i])
-	})
-	writeFs(path, body)
+	function replaceAddress(path) {
+		let readAble = fs.createReadStream(path)
+		let body = ''
+		readAble.on('data', (chunk) => {
+		  body += chunk
+		})
+		readAble.on('end', () => {
+			matchData(path, data, body)
+		})
+	}
+
+
+	function matchData(path, data, body) {
+		let replaceBody = {}
+		data.forEach((obj, i) => {
+			if(obj.type === 'script' || obj.type === 'link' || obj.type === 'img') {
+				let bodyMatch = body.match(new RegExp(`<${obj.type}.*?>`, 'g'))
+				if(bodyMatch) {
+					bodyMatch.forEach((item, index) => {
+						let itemMatch = item.match(/(src|href)\s*=\s*["|'].*?["|']/g)
+						if(itemMatch) {
+							itemMatch.forEach((data, i) => {
+								let matchItem = data.match(/(["|']).*\//g)[0].replace(/\s/g, '').slice(1)
+								if(!replaceBody[matchItem]) {
+									replaceBody[matchItem] = obj.point
+								}
+							})
+						}
+					})
+				}
+			} else if(obj.type === 'background') {
+				let bodyMatch = body.match(/url\(.*?\)/g)
+				if(bodyMatch) {
+					bodyMatch.forEach((item, index) => {
+						let itemMatch = item.match(/\(.*\//g)[0].replace(/\s/g, '').slice(1)
+						if(!replaceBody[itemMatch]) {
+							replaceBody[itemMatch] = obj.point
+						}
+					})
+				}
+
+			}
+		})
+		replaceSepical(path, body, replaceBody)	
+	}
+
+	function replaceSepical(path, body, replaceBody) {
+		Object.keys(replaceBody).sort((a,b) => b.length - a.length).forEach((item, index) => {
+			let i = item,
+				itemReplace
+			if(item.match(/\.\//g)) {
+				item = item.replace(/\./g, '\\\.')
+			} 
+			itemReplace = new RegExp(item, 'g')	
+			body = body.replace(itemReplace, replaceBody[i])
+		})
+		writeFs(path, body)
+	}
+	function writeFs(path, body) {
+		fs.writeFile(path, body, (err) => {
+		  	if (err) throw err;
+		})
+	}
+	// fsPathSys(path, dataKey)
 }
-function writeFs(path, body) {
-	fs.writeFile(path, body, (err) => {
-	  	if (err) throw err;
-	})
-}
-fsPathSys(path, dataKey)
+exports.fsPathRepeat = fsPathRepeat
---
+++ fsPathSys/server.js
@@ -0,0 +1,105 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+const path = require('path')
+const multiparty = require('multiparty')
+const zlib = require('zlib')
+const gzip = zlib.createGzip()
+const unzip = zlib.createUnzip()
+const FS = require('./fs.js')
+const MIME_TYPE = {
+    "css": "text/css",
+    "gif": "image/gif",
+    "html": "text/html",
+    "ico": "image/x-icon",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "js": "text/javascript",
+    "json": "application/json",
+    "pdf": "application/pdf",
+    "png": "image/png",
+    "svg": "image/svg+xml",
+    "swf": "application/x-shockwave-flash",
+    "tiff": "image/tiff",
+    "txt": "text/plain",
+    "wav": "audio/x-wav",
+    "wma": "audio/x-ms-wma",
+    "wmv": "video/x-ms-wmv",
+    "xml": "text/xml"
+}
+const server = http.createServer((req, res) => {
+	serverStatic(req, res)
+}).listen(3044)
+
+function serverStatic(req, res) {
+    let filePath
+    if(~req.url.indexOf('api')){
+    	let ip = req.socket.remoteAddress.slice(7),
+    		nowTime = new Date().getTime(),
+            id = ip + nowTime
+    	cons('用户ip：' + ip)
+ 		resolveData(req, res, id)
+    } else {
+        filePath = "." + url.parse(req.url).pathname
+        sendFile(filePath, res)
+    }
+}
+function resolveData(req, res, id) {
+    cons('开始处理数据')
+    let form = new multiparty.Form()
+    form.parse(req, function (err, fields, files) {
+        let filename = files['file'][0].originalFilename,
+            targetPath = __dirname + '/files/' + id
+
+         console.log(filename)
+        if (filename) {
+        	cons('文件解压缩')
+            let inp = fs.createReadStream(files['file'][0].path),
+            	out = fs.createWriteStream(targetPath)
+        	inp.pipe(unzip).pipe(out)
+            inp.on('end', () => {
+            	cons('解压缩完成')
+            })
+            
+            
+        } else {
+            let errData = {
+                status: 400,
+                msg: 'wrong params'
+            }
+            res.writeHead(400, {'Content-type':'application/json'})
+            res.end(JSON.stringify(errData))
+        }
+          
+    })
+}
+function sendFile(filePath, res) {
+    fs.open(filePath, 'r+', function(err){
+        if(err){
+            send404(res)
+        }else{
+            let ext = path.extname(filePath)
+            ext = ext ? ext.slice(1) : 'unknown'
+            let contentType = MIME_TYPE[ext] || "text/plain"
+            fs.readFile(filePath,function(err,data){
+                if(err){
+                    send500(res)
+                }else{
+                    res.writeHead(200,{'content-type':contentType})
+                    res.end(data)
+                }
+            })
+        }
+    })
+}
+
+function cons(str) {
+	console.log(str)
+	console.log(`------------------------------------------`)
+}
+function send404(res){
+    res.end("<h1 style='text-align:center'>404</h1><p style='text-align:center'>file not found</p>")
+}
+function send500(res){
+    res.end("<h1>500</h1>服务器内部错误！")
+}

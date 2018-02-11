45291f9abf7bd5407e475b30de9abeffda05923c
Aaaaaaaty <2103887953@qq.com>
2018-02-11 16:15:41 +0800

    update5327881713013374

--- Algorithm/kmp/kmp.js
+++ Algorithm/kmp/kmp.js
@@ -1,18 +1,29 @@
 //传统算法
 
 
-var baseStr = 'BBC ABCDAB ABCDABCDABDE'
-var targetStr = 'ABCDABD'
+var baseStr = ''
+var targetStr = 'ABCDEFG'
+for(var i = 0; i < 3; i++) {
+    baseStr += 'ABCDEF '
+}
+baseStr += 'ABCDEFG' 
+
+function baseDom(str) {
+
+}
+function targetDom(str, index) {
+
+}
 function mapStr(base, target) {
-    base = base.split('')
-    target = target.split('')
     var isMatch = []
     console.time('normal')
+    var times = 0
     for(var i = 0; i < base.length; i++) {
+        times++
         for(var j = 0; j < target.length; j++) {
             if (i + target.length <= base.length) {
-                if (target[j] === base[i + j]) {
-                    isMatch.push(target[j])
+                if (target.charAt(j) === base.charAt(i + j)) {
+                    isMatch.push(target.charAt(j))
                 } else {
                     break
                 }
@@ -20,6 +31,7 @@ function mapStr(base, target) {
         }
         if(isMatch.length === target.length) {
             console.timeEnd('normal')
+            console.log('移位次数:', times)
             return i
         }
         isMatch = []
@@ -33,6 +45,7 @@ function mapStr(base, target) {
 //kmp
 function pmtArr(target) {
     var pmtArr = []
+    target = target.split('')
     for(var j = 0; j < target.length; j++) {
         var pmt = target
         var pmtNum = 0
@@ -50,16 +63,16 @@ function pmtArr(target) {
 }
 
 function mapKMPStr(base, target) {
-    base = base.split('')
-    target = target.split('')
     var isMatch = []
     var pmt = pmtArr(target)
     console.time('kmp')
+    var times = 0
     for(var i = 0; i < base.length; i++) {
+        times++
         for(var j = 0; j < target.length; j++) {
             if(i + target.length <= base.length) {
-                if (target[j] === base[i + j]) {
-                    isMatch.push(target[j])
+                if (target.charAt(j) === base.charAt(i + j)) {
+                    isMatch.push(target.charAt(j))
                 } else {
                     if(!j) break
                     var skip = pmt[j - 1]
@@ -70,6 +83,7 @@ function mapKMPStr(base, target) {
         }
         if(isMatch.length === target.length) {
             console.timeEnd('kmp')
+            console.log('移位次数:', times)
             return i
         }
         isMatch = []
@@ -78,6 +92,5 @@ function mapKMPStr(base, target) {
     return -1
     
 }
-console.log(mapKMPStr(baseStr, targetStr))
-console.log(mapStr(baseStr, targetStr))
-
+mapKMPStr(baseStr, targetStr)
+mapStr(baseStr, targetStr)

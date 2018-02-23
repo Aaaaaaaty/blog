828d708994516171f9d8a1219946d3cb5d1690c7
Aaaaaaaty <2103887953@qq.com>
2018-02-23 11:33:03 +0800

    update7443056068866809

--- Algorithm/kmp/kmp.js
+++ Algorithm/kmp/kmp.js
@@ -1,18 +1,50 @@
 //传统算法
 
 
-var baseStr = ''
-var targetStr = 'ABCDEFG'
-for(var i = 0; i < 3; i++) {
-    baseStr += 'ABCDEF '
+var baseStr = 'BBC ABCDAB ABCDABCDABDE' 
+var targetStr = 'ABCDABD'
+var diff = 30
+var caller = []
+var callerKmp = []
+
+function init() {
+    printDom('baseStr', baseStr, 'base')
+    printDom('baseStr-kmp', baseStr, 'base-kmp')
+    printDom('targetStr', targetStr, 'target')
+    printDom('targetStr-kmp', targetStr, 'target-kmp')
+    mapKMPStr(baseStr, targetStr)
+    mapStr(baseStr, targetStr)
+    callerKmp.forEach(function(item, index) {
+        setTimeout(function(obj) {
+            moveDom('targetStr-kmp', obj, index)
+        }.bind(this, item), index * 1000)
+    })
+    caller.forEach(function(item, index) {
+        setTimeout(function(obj) {
+            moveDom('targetStr', obj, index)
+        }.bind(this, item), index * 1000)
+    })
 }
-baseStr += 'ABCDEFG' 
-
-function baseDom(str) {
-
+function printDom($id, str, type) {
+    var htmlStr = ''
+    str.split('').forEach(function(item, index) {
+        var id = type + index
+        htmlStr = '<div id='+ id +' class="div">'+ item +'</div>'
+        $('#' + $id).append(htmlStr)
+        $('#' + id).css({"left": diff * index})
+        if(type === 'target' || type === 'target-kmp') $('#' + id).css({"top": 40})  
+    })
+    
 }
-function targetDom(str, index) {
 
+function moveDom(id, data, num) {
+    $('#' + id).find(".div").each(function (index, item) {
+        $(item).css({"backgroundColor": "#dcdcdc"})
+        $(item).css({ 'left': diff * (data.index + index) + 'px' })
+        if(data.matchArr.length && index <= data.matchArr.length - 1) {
+            $(item).css({"backgroundColor": "red"})
+        }
+    }).parent().siblings('.num').find('span').html(num)
 }
 function mapStr(base, target) {
     var isMatch = []
@@ -20,6 +52,7 @@ function mapStr(base, target) {
     var times = 0
     for(var i = 0; i < base.length; i++) {
         times++
+        var tempIndex = 0
         for(var j = 0; j < target.length; j++) {
             if (i + target.length <= base.length) {
                 if (target.charAt(j) === base.charAt(i + j)) {
@@ -29,11 +62,18 @@ function mapStr(base, target) {
                 }
             }
         }
+        var data = {
+            index: i,
+            matchArr: isMatch
+        }
+        caller.push(data)
+        if(tempIndex) i = tempIndex
         if(isMatch.length === target.length) {
             console.timeEnd('normal')
             console.log('移位次数:', times)
             return i
         }
+        
         isMatch = []
     }
     console.timeEnd('normal')
@@ -69,6 +109,7 @@ function mapKMPStr(base, target) {
     var times = 0
     for(var i = 0; i < base.length; i++) {
         times++
+        var tempIndex = 0
         for(var j = 0; j < target.length; j++) {
             if(i + target.length <= base.length) {
                 if (target.charAt(j) === base.charAt(i + j)) {
@@ -76,11 +117,17 @@ function mapKMPStr(base, target) {
                 } else {
                     if(!j) break
                     var skip = pmt[j - 1]
-                    i += skip - 1
+                    tempIndex = i + skip - 1
                     break 
                 }
             }
         }
+        var data = {
+            index: i,
+            matchArr: isMatch
+        }
+        callerKmp.push(data)
+        if(tempIndex) i = tempIndex
         if(isMatch.length === target.length) {
             console.timeEnd('kmp')
             console.log('移位次数:', times)
@@ -89,8 +136,9 @@ function mapKMPStr(base, target) {
         isMatch = []
     }
     console.timeEnd('kmp')
+    
     return -1
     
 }
-mapKMPStr(baseStr, targetStr)
-mapStr(baseStr, targetStr)
+
+
